162ffaa6b03a9fa01ae75a2343e0de7dc4e43f75
Adekunle Seun <adekunleseun001@gmail.com>
2017-05-10 13:57:50 +0100

    Refactor code

--- app/word_count.js
+++ app/word_count.js
@@ -5,16 +5,16 @@ const words = (str) =>{
 
   // Includes punctuation, international characters, removes whitespaces on both ends and split to form an array
   let wordArr = str.replace(/'(?!\w)|\\'\s/g, "").replace(/\s\'(?=\w)|,|\n|\t|\b  \b/g, " ").trim().split(" ");
-  
-  wordArr.forEach(function (word){
+  // Treat each element in the array individually
+  wordArr.forEach((word) =>{
   if (Object.hasOwnProperty.call(result, word)){
-    result[word]+=1;
-   } else {
-  result[word]=1;
+    result[word] += 1;
+   } 
+  else {
+    result[word] = 1;
   }   
 
   });
-
   return result;
 };
 
